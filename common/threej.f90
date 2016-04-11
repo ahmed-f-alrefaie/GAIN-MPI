e682c886d38acda1668a2d060853404a8f9f9173
Ahmed Al-Refaie <ucapfal@cobweb-login.local>
2016-04-11 11:56:47 +0100

    Forgot to add the FORTRAN threeJ symbol function

---
+++ common/threej.f90
@@ -0,0 +1,157 @@
+module FORT_FUNC
+
+
+	implicit none
+		public three_j
+  integer, parameter :: sik         = selected_int_kind(4)       ! Small integers
+  integer, parameter :: ik          = selected_int_kind(8)       ! "Normal" integers. This must map on
+                                                                 ! C "int" type, or the DX interface won't
+                                                                 ! work correctly.
+  integer, parameter :: hik         = selected_int_kind(16)      ! "Pointer" integers - sufficient to store
+                                                                 ! memory address
+  integer, parameter :: drk         = selected_real_kind(12,25)  ! "Double" reals and complex (complexi? :-)
+  integer, parameter :: rk          = selected_real_kind(12,25)  ! "Normal" reals and complex (complexi? :-)
+  integer, parameter :: ark         = selected_real_kind(25,32)  ! "Accurate" reals and complex (complexi? :-)
+  contains
+   function three_j(j1,j2,j3,k1,k2,k3) bind (c, name='c_three_j')
+  use,intrinsic :: iso_c_binding
+  implicit none
+
+      real(rk) :: three_j
+      integer(ik),intent(in) :: j1,j2,j3,k1,k2,k3
+      real(rk) :: a,b,c,al,be,ga
+      !
+      integer(ik):: newmin,newmax,new,iphase
+      real(rk)   :: delta,clebsh,minus
+      real(rk)   :: term,term1,term2,term3,summ,dnew,term4,term5,term6,delta_log,term16,termlog
+
+      a = j1
+      b = j2
+      c = j3
+      al= k1
+      be= k2
+      ga= k3
+
+      three_j=0
+!
+!     (j1+j2).ge.j and j.ge.abs(a-b)    -m=m1+m2    j1,j2,j.ge.0
+!     abs(m1).le.j1    abs(m2).le.j2   abs(m).le.j
+!
+      if(c.gt.a+b) return
+      if(c.lt.abs(a-b)) return
+      if(a.lt.0.or.b.lt.0.or.c.lt.0) return
+      if(a.lt.abs(al).or.b.lt.abs(be).or.c.lt.abs(ga)) return
+      if(-1.0_rk*ga.ne.al+be) return
+!
+!
+!     compute delta(abc)
+!
+!     delta=sqrt(fakt(a+b-c)*fakt(a+c-b)*fakt(b+c-a)/fakt(a+b+c+1.0_rk))
+      delta_log = faclogf(a+b-c)+faclogf(a+c-b)+faclogf(b+c-a)-faclogf(a+b+c+1.0_rk)
+      !
+      delta=sqrt(exp(delta_log)) 
+!
+!
+      !term1=fakt(a+al)*fakt(a-al)
+      !term2=fakt(b-be)*fakt(b+be)
+      !term3=fakt(c+ga)*fakt(c-ga)
+      !
+      !term=sqrt( (2.0_rk*c+1.0_rk)*term1*term2*term3 )
+      !
+      !
+      term1=faclogf(a+al)+faclogf(a-al)
+      term2=faclogf(b-be)+faclogf(b+be)
+      term3=faclogf(c+ga)+faclogf(c-ga)
+      !
+      termlog = ( term1+term2+term3+delta_log )*0.5_rk
+ 
+      term=sqrt( (2.0_rk*c+1.0_rk) )
+!
+!
+!     now compute summation term
+!
+!     sum to get summation in eq(2.34) of brink and satchler.  sum until
+!     a term inside factorial goes negative.  new is index for summation
+!     .  now find what the range of new is.
+!
+!
+      newmin=idnint(max((a+be-c),(b-c-al),0.0_rk))
+      newmax=idnint(min((a-al),(b+be),(a+b-c)))
+!
+!
+      summ=0
+!
+!
+      do new=newmin,newmax
+        !
+        dnew=real(new,rk)
+        !
+        term4=faclogf(a-al-dnew)+faclogf(c-b+al+dnew)
+        term5=faclogf(b+be-dnew)+faclogf(c-a-be+dnew)
+        term6=faclogf(dnew)+faclogf(a+b-c-dnew)
+        !
+        term16=termlog-(term4+term5+term6)
+        !
+        summ=summ+(-1.0_rk)**new*exp(term16)
+        !
+      enddo
+!
+!     so clebsch-gordon <j1j2m1m2ljm> is clebsh
+!
+      clebsh=term*summ ! /sqrt(10.0_rk)
+!
+!     convert clebsch-gordon to three_j
+!
+      iphase=idnint(a-b-ga)
+      minus = -1.0_rk
+      if (mod(iphase,2).eq.0) minus = 1.0_rk
+      three_j=minus*clebsh/term
+
+!     threej=(-1.d0)**(iphase)*clebsh/sqrt(2.0_rk*c+1.d0)
+!
+!
+   end function three_j
+
+
+
+   function fakt(a) result (f)
+
+      real(rk),intent(in) :: a
+      real(rk)            :: ax,f
+      integer(ik)         :: i,ic
+!
+
+!
+      ax=a
+      f=1.0_rk
+      if(abs(ax)<1.d-24) return
+      f=.1d0
+      if(ax.lt.0.d0) then 
+         write (*,"(1h0,' fkt.err  negative argument for functi on fakt. argument = ',e12.5)") ax
+         stop 'fkt.err  negative argument'
+      endif 
+      !
+      ic=idnint(ax)
+      ax=ax/10.0_rk
+      f=ax
+      do  i=1,ic-1
+        f=f*(ax-real(i,rk)*0.1_rk)
+      enddo
+
+    end function fakt
+
+  function faclogf(a)   result (v)
+    real(rk),intent(in) ::  a
+    real(ark)              :: v 
+    integer(ik) j,k
+
+    v=0
+    k=nint(a)
+    if(k>=2) then 
+      do j=2,k
+         v=v+log(real(j,ark))
+      enddo 
+    endif 
+    
+  end function faclogf
+end module FORT_FUNC
--- main.cpp
+++ main.cpp
@@ -274,66 +274,6 @@ int main(int argc, char** argv){
 		Timer::getInstance().PrintTimerInfo();
 	}
 
-
-/*
-
-	//Read the input file
-	TroveInput troveinput;
-	troveinput.ReadInput(input_filename);
-
-	TroveStates troveStates(troveinput);
-
-	troveStates.ReadStates();
-
-	printf("Largest vector is of size: %d\n",troveStates.GetNSizeMax());
-
-
-	
-	GpuManager gpu(0,1);
-
-	TroveBasisSet basisSetj0(0,troveinput.GetNSym(),troveinput.GetSymmetryDegen());
-	TroveBasisSet basisSetj1(1,troveinput.GetNSym(),troveinput.GetSymmetryDegen());
-
-	
-
-
-	basisSetj0.Initialize();
-	basisSetj1.Initialize();
-
-	std::vector<int> TryNTot = basisSetj1.GetNTotal();
-	for(int i = 0; i < TryNTot.size(); i++){
-
-		printf("%i\n",TryNTot[i]);
-
-	}
-
-	gpu.InitializeAndTransferConstants(troveinput.GetMaxJ(),troveinput.GetNSym(),troveinput.GetSymMaxDegen());
-	
-	gpu.AllocateVectors(troveinput.GetNJ(),troveStates.GetNSizeMax(),BasisSet::GetDimenMax());
-
-	gpu.TransferBasisSet(&basisSetj0);
-	gpu.TransferBasisSet(&basisSetj1);
-
-	gpu.TransferInflation(basisSetj0.GetContr(), basisSetj0.GetIJTerms(),basisSetj0.GetDimensions(),basisSetj0.GetMaxSymCoeffs(),
-			basisSetj0.GetMatSize(),basisSetj0.GetRepres(),basisSetj0.GetRepresN(),basisSetj0.GetNTotal(),troveinput.GetSymmetryDegen());
-
-	gpu.TransferInflation(basisSetj1.GetContr(), basisSetj1.GetIJTerms(),basisSetj1.GetDimensions(),basisSetj1.GetMaxSymCoeffs(),
-			basisSetj1.GetMatSize(),basisSetj1.GetRepres(),basisSetj1.GetRepresN(),basisSetj1.GetNTotal(),troveinput.GetSymmetryDegen());
-
-
-	gpu.TransferDipole(&test_dipole,0);
-
-
-	test_dipole.RemoveDipole();
-
-	EigenVector eigen(troveinput);
-
-	eigen.CacheEigenvectors(&troveStates);
-
-
-	printf("DimenMax: %i\n",BasisSet::GetDimenMax());
-
-*/
 	MPI_Finalize();
 	
 
